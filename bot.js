48d4e7ec9176f11311630811ac4f7e1794da1b23
R3XP <rex_p@web.de>
2017-08-11 15:44:45 +0200

    commited

--- bot.js
+++ bot.js
@@ -10,11 +10,17 @@ const bot = new Discord.Client()
 
 bot.infopath = "./assets/BOTinf.json"
 bot.infs = require(bot.infopath)
-bot.testuser = bot.infs.crap.owner
 bot.infos = bot.infs.infos
+
+bot.LoggingfilePath = bot.infos.loggingfile
+bot.logs = require(bot.LoggingfilePath)
+
+
 bot.Logintoken = bot.infos.token
-bot.prefix = bot.infos.prefix
+bot.prefix = bot.infos.defaultprefix
 bot.version = bot.infos.version
+
+
 bot.ALang = "";
 console.log('Version:', bot.version)
 
@@ -31,6 +37,8 @@ bot.login(bot.Logintoken)
 
 
 bot.on('ready', () => {
+    
+console.log("------------------------------------")
     console.log("I'M ready now, master")
 
     var testuser = bot.guilds.get("340168395058184202").members.get("137594857777659904")
@@ -49,90 +57,93 @@ bot.on('message', message => {
 
     /*hier kommt der main Part hin, der ausgeführt wird, sobald eine Nachricht gesendet wird!*/
 
-
-
     let args = message.content.split(" ")
     let cmd = args[0].toLowerCase()
 
-    let MAuthorName = message.author.name
+    let MAuthorName = message.author.username
     let MUserinfo = bot.infs.users[MAuthorName]
 
     /*Wenn true, dann ist der Kanal in der die Nachricht gesendet wurde ein Gilden Text kanal*/
     if(message.channel.type == "text") {
 
+        let AChannelType = "text"
+
         let guildinf = bot.infs.guilds[message.guild.id]
         let MGuildId = message.guild.id
 
-        console.log(MUserinfo);
-
         //Fügt die neue Guilde zur Liste hinzu, wenn sie noch nicht vorhanden ist.
-        if(!message.author.bot) {
-            if(!guildinf) {
+        // if(!message.author.bot) {
+        //     if(!guildinf) {
 
-                let tolog = bot.infs.guilds[message.guild.id] = {}
-                tolog.name = message.guild.name
-                tolog.id = message.guild.id
+        //         let tolog = bot.infs.guilds[message.guild.id] = {}
+        //         tolog.name = message.guild.name //notiert den gilden namen
+        //         tolog.id = message.guild.id     //notiert die Gilden id
 
-                message.guild.createChannel("Bot-channel", "text")
-
-                .then(() => {
-                    let botchannel1 = message.guild.channels.find('name', 'bot-channel')
+        //         message.guild.createChannel("Bot-channel", "text")
+        //         .then(() => { /*Legt den Botchannel der Gilde fest*/ 
+        //             let botchannel1 = message.guild.channels.find('name', 'bot-channel')
                     
-                    tolog.BotChannel = botchannel1.id
+        //             tolog.BotChannel = botchannel1.id
 
 
 
-                    botchannel1.overwritePermissions(message.guild.defaultRole, {
-                        SEND_MESSAGES: false
-                    })
+        //             botchannel1.overwritePermissions(message.guild.defaultRole, {
+        //                 SEND_MESSAGES: false
+        //             })
 
 
 
-                    bot.channels.get(tolog.BotChannel).send("Hello @everyone! I'm the ``Rex o' Bots`` bot and I'm still Wip by my wonderful coder " +bot.owner +"! \nThis Channel was created by me, and is meant to show you the upgrade / patch nodes!")
-                    bot.channels.get(tolog.BotChannel).send(`My version is: ${bot.version}.`)
-                    bot.channels.get(tolog.BotChannel).send("You can configure the language by typing ``" +bot.prefix +"conf lang [ger/eng]`` in any channel!")
-                    bot.channels.get(tolog.BotChannel).send("For more information just type ``" +bot.prefix +"help`` in dm.")
+        //             bot.channels.get(tolog.BotChannel).send("Hello @everyone! I'm the ``Rex o' Bots`` bot and I'm still Wip by my wonderful coder " +bot.owner +"! \nThis Channel was created by me, and is meant to show you the upgrade / patch nodes!")
+        //             bot.channels.get(tolog.BotChannel).send(`My version is: ${bot.version}.`)
+        //             bot.channels.get(tolog.BotChannel).send("You can configure the language by typing ``" +bot.prefix +"conf lang [ger/eng]`` in any channel!")
+        //             bot.channels.get(tolog.BotChannel).send("For more information just type ``" +bot.prefix +"help`` in dm.")
 
                     
-                })
-                .catch(console.error);
+        //         })
+        //         .catch(console.error);
 
 
-                tolog.language = "english"
-                let search = message.guild.members.array()
-                for(i in search) {
 
-                    console.log(i)
+        //         tolog.konfig = {}
+        //         tolog.konfig.language = "english"
+        //         tolog.konfig.version = bot.version
 
-                    if(!bot.infs.users[search[i].id]) {
-                        bot.infs.users[search[i].id] = {}
-                    }
+
+
+        //         // let search = message.guild.members.array()
+        //         // for(i in search) {
+
+        //         //     console.log(i)
+
+        //         //     if(!bot.infs.users[search[i].id]) {
+        //         //         bot.infs.users[search[i].id] = {}
+        //         //     }
                 
-                    let loguser = bot.infs.users[search[i].id]
-                    loguser.id = search[i].id
-                    loguser.name = search[i].username
-                    loguser.language = "eng"
+        //         //     let loguser = bot.infs.users[search[i].id]
+        //         //     loguser.id = search[i].id
+        //         //     loguser.name = search[i].username
+        //         //     loguser.language = "eng"
 
-                    let userguilds = search[i].user.guilds.array()
-                    let guildarr;
+        //         //     let userguilds = search[i].user.guilds.array()
+        //         //     let guildarr;
 
-                    for(i in userguilds) {
-                        guildarr[i] = userguilds[i].name 
-                    }
+        //         //     for(i in userguilds) {
+        //         //         guildarr[i] = userguilds[i].name 
+        //         //     }
 
-                    loguser.guilds = guildarr
+        //         //     loguser.guilds = guildarr
 
-                }
-
-                
-
+        //         // }
 
                 
-                bot.logit()
-            }
 
 
-        }
+                
+        //         bot.logit()
+        //     }
+
+
+        // }
         bot.infs = require(bot.infopath)
 
 
@@ -145,10 +156,18 @@ bot.on('message', message => {
 
 
         if(commandIs(cmd, "delete")) Bcommanddelete(message, args);
-    
+        if(commandIs(cmd, "help")) {
+            
+            if(bot.infs.users[message.author.id].lang == "ger"){
+                message.author.send("Dieser befehl ist nur per direkt nachricht verfügbar!")
+            } else {
+                message.author.send("This command must be send in a dm channel!")
+            }
+        }
 
     } else { /*Sonst ist es ein Direct message kanal*/
 
+        let AChannelType = "dm"
         if(commandIs(cmd, "help")) Bcommandhelp(message, args);  
 
     }
@@ -157,8 +176,16 @@ bot.on('message', message => {
 
     if(!message.author.bot && !MUserinfo) {
 
-        console.log('got no infos about: ' +message.author.username)
+        console.log('Getting infos about user: ' +message.author.username)
 
+        let tolog = bot.infs.users[message.author.id] = {}
+        tolog.name = MAuthorName
+        tolog.id = message.author.id
+        tolog.fullname = MAuthorName +"#" +message.author.discriminator
+        console.log(bot.infs.users)
+
+
+        // bot.logit()
     }
 
 
@@ -223,6 +250,42 @@ bot.logit = function() {
 
 
 
+bot.txtlog = function(logdata) {
+
+    let Day = new Date().getDate()
+    let Month = new Date().getMonth()
+    let myDate = `${Day}.${Month}`
+    let Hour = new Date().getHours()
+    let Minute = new Date().getMinutes()
+    let Secs = new Date().getSeconds()
+    let myTime = `[${Hour}:${Minute} - ${Secs}]`
+
+
+    
+    if(!bot.logs[Month]) {
+        bot.logs[Month] = {}
+    }
+    let logthis = bot.logs[Month]
+
+    if(!logthis[myDate]) {
+        logthis[myDate] = []
+    }
+    logthis = logthis[myDate]
+
+    
+    logthis[logthis.length] = myTime + " " +logdata
+
+
+
+    jlog(bot.LoggingfilePath, bot.logs)
+
+}
+
+
+
+
+
+
 function Bcommanddelete(message, args) {
 
     if(args.length >=3){
@@ -262,9 +325,11 @@ function Bcommandhelp(message, args) {
             }
         }
 
-        
-        message.channel.send(`Hallo ${message.author} und danke, dass du mich benutzt!\nBitte gib den command an, für den du Hilfe haben möchtest: \n` +"``" +bot.commandstring +"``")
-        
+        if(bot.ALang == "ger") {
+            message.channel.send(`Hallo ${message.author} und danke, dass du mich benutzt!\nBitte gib den command an, für den du Hilfe haben möchtest: \n` +"``" +bot.commandstring +"``")
+        } else {
+            message.channel.send(`Hello ${message.author} thans for using me!\nPlease tell me, for wich command you need help: \n` +"``" +bot.commandstring +"``")
+        }
 
 
     } else if(args.length == 2) {
@@ -288,6 +353,8 @@ function Bcommandhelp(message, args) {
 
        if(bot.ALang == "ger"){
            message.channel.send("Das waren zu viele Argumente!")
+        } else {
+            message.channel.send("Too many arguments!")
         }
 
     }
